ea710daf20440d94c9d7f28909f9a491bef785ab
Vasilevsky <f.vasilevsky@a1qa.com>
2023-06-02 21:13:08 +0200

    The project has been migrated to Selenium 4

--- src/browser/local_browser_factory.py
+++ src/browser/local_browser_factory.py
@@ -1,6 +1,12 @@
 import typing as ty
 
 from selenium.webdriver import Chrome, Firefox, Ie, Edge
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.ie.service import Service as IEService
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import IEDriverManager
@@ -28,18 +34,22 @@ class LocalBaseBrowserFactory(BaseBrowserFactory):
         capabilities = driver_settings.get_capabilities()
         if browser_name == BrowserName.CHROME.value.lower():
             executable_path = ChromeDriverManager(version=driver_version).install()
-            self.__driver = Chrome(executable_path=executable_path, options=capabilities)
+            service = ChromeService(executable_path)
+            self.__driver = Chrome(service=service, options=capabilities)
 
         elif browser_name.lower() == BrowserName.FIREFOX.value.lower():
             executable_path = GeckoDriverManager(version=driver_version).install()
-            self.__driver = Firefox(executable_path=executable_path, options=capabilities)
+            service = FirefoxService(executable_path=executable_path)
+            self.__driver = Firefox(service=service, options=capabilities)
 
         elif browser_name.lower() == BrowserName.INTERNET_EXPLORER.value.lower():
             executable_path = IEDriverManager(version=driver_version).install()
-            self.__driver = Ie(executable_path=executable_path)
+            service = IEService(executable_path=executable_path)
+            self.__driver = Ie(service=service)
 
         elif browser_name.lower() == BrowserName.EDGE.value.lower():
             executable_path = EdgeChromiumDriverManager(version=driver_version).install()
-            self.__driver = Edge(executable_path)
+            service = EdgeService(executable_path=executable_path)
+            self.__driver = Edge(service=service)
 
         return self.__driver
